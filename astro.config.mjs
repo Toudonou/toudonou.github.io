bf73c67654837e8e324cb7af3fb37aa356e94fca
toudonou <hnounagnonoswald@gmail.com>
2026-06-13 08:43:34 +0200

    Update Astro configuration for static output and trailing slashes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://toudonou.github.io",
+  output: "static",
+  trailingSlash: "always",
   vite: {
     plugins: [tailwindcss()],
   },
